aabd46f1e830a4045846f27bd0281c8c90bc3064
georgegyan <georgegyankwame72@gmail.com>
2026-02-25 15:09:11 +0000

    feat: complete frontend setup

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -2,7 +2,20 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
+  server: {
+    port: 5173,
+    open: true,
+    proxy: {
+      '/api': {
+        target: 'http://localhost:8000',
+        changeOrigin: true,
+        secure: false,
+      }
+    }
+  },
+  css: {
+    postcss: './postcss.config.js',
+  },
 })
